2f70a119b7f19a6e282703805d4b1f96239c33a6
Harish M <harishmadamanchi95@gmail.com>
2020-03-28 14:36:14 +0530

    added the intent navigation to login now link

--- app/src/main/java/com/example/myapplication/SignupActivity.java
+++ app/src/main/java/com/example/myapplication/SignupActivity.java
@@ -1,14 +1,17 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -16,6 +19,16 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        AppCompatTextView login = (AppCompatTextView) findViewById(R.id.loginlink);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(v.getContext(),MainActivity.class);
+                startActivity(login);
+                finish();
+            }
+        });
 
     }
 
